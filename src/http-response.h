48f226673d0509d49b519fac1e9d49a87eef4fb9
Alexander Karlsson <alka1905@student.miun.se>
2022-01-12 16:34:08 +0100

    Renamed the location of the resources requested by the user, like html files etc. Old name: paths, New name: resources

--- src/http-response.h
+++ src/http-response.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#define RELATIVE_PATH "./paths"  
+#define RELATIVE_PATH "./resources"  
 #define DEFAULT_FILE "/index.html"
 #define LINE_MAX_SIZE 256
 #define CONNECTION_CLOSE "close"
@@ -14,13 +14,4 @@ int sendHttpResponse(int socket, int statuscode, const char* connection, const c
 int loadResource(int socket, char* path, char** buffer, int* status_code, char** error_message);
 int parse_requestline(char* line, int len, char** command, char** server, char** path, char** protocol, char** port);
 int isdigit(char ch);
-
-
-// TODO: Remove these once the handleClient function has been modified to always use sendHttpResponse
-//#define DEFAULT_RESPONSE_HEADER_200 "HTTP/1.1 200 OK\r\nConnection: Closed\r\nContent-Type: text/html; charset=iso-8859-1\r\n\n"
-//#define DEFAULT_RESPONSE_HEADER_400 "HTTP/1.1 400 Bad Request\r\nConnection: Closed\r\n\n"
-//#define DEFAULT_RESPONSE_HEADER_500 "HTTP/1.1 500 Internal Server Error\r\nConnection: Close\r\n\n"
-//int createResponseHeader(char** header, int statuscode, const char* customdesc);
-//void sendClientError(int socket);  
-// void sendServerError(int socket);  
 
